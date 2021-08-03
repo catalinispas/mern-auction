4018f44a20e41c75efd4766540a9849e08d849b5
Catalin Ispas <70150418+catalinispas@users.noreply.github.com>
2021-08-03 14:23:24 +0300

    can bid / incomplete

--- routes/api/items.js
+++ routes/api/items.js
@@ -30,4 +30,35 @@ router.post('/', auth, (req, res) => {
   newItem.save().then((item) => res.json(item));
 });
 
+// @route   GET api/items/:id
+// @desc    Gets one item by id
+// @access  Public
+router.get('/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    return res.json(item);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ msg: 'Item not found' });
+  }
+});
+
+// @route   GET api/items/:id/bid
+// @desc    Places a bid on a item by id
+// @access  Private
+router.post('/:id/bid', auth, async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    const newBid = {
+      userId: req.user.id,
+      amount: req.amount,
+    };
+    item.bids.push(newBid);
+    return res.json(item);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ msg: 'Item not found' });
+  }
+});
+
 module.exports = router;
